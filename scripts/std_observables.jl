9055329ff253f9462d212a6568abfda1986c84e7
Fabian Zierler <fabian.zierler@a1.net>
2025-02-18 17:27:12 +0000

    add std observables for importance sampling without error

---
+++ scripts/std_observables.jl
@@ -0,0 +1,31 @@
+using Pkg; Pkg.activate(".")
+using BenchmarkTools
+using HDF5
+using StatsBase
+
+# For higher moments I use the didcated functions because they are faster
+# I have checked that the numerical precision is sufficient
+binder_cumulant(x) = 1 - moment(x,4,0.0) / mean(f->f^2,x)^2 / 3
+poly_susceptibility(x,Nl) = var(abs.(x))*Nl^3
+specific_heat_plaq(x,Nl,Nt) = var(x)*6Nl^3*Nt
+
+# TODO: Give variance of the mean of those statistical uncertainties analytically
+function std_observables(f,ens)
+    plaq = f[ens]["plaquette"][]
+    poly = f[ens]["polyakov_loop"][]
+    Nt = f[ens]["Nt"][]
+    Nl = f[ens]["Nl"][]
+
+    binder_plaq = binder_cumulant(plaq)
+    poly_sus = poly_susceptibility(poly,Nl)
+    sh_plaq  = specific_heat_plaq(plaq,Nl,Nt)
+    poly_vev = mean(abs,poly)
+    plaq_vev = mean(plaq)
+    return binder_plaq, poly_sus, sh_plaq, poly_vev, plaq_vev
+end
+
+h5file = "test.hdf5"
+f      = h5open(h5file)
+ens    = "ImportanceSampling/4x20/7.32/"
+
+binder_plaq, poly_sus, sh_plaq, poly_vev, plaq_vev = std_observables(f,ens)
