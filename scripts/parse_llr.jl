4432e9017deb54d200333112e14c451e45ea73b9
Fabian Zierler <fabian.zierler@a1.net>
2025-03-04 17:21:59 +0000

    add parsing for dS0

--- scripts/parse_llr.jl
+++ scripts/parse_llr.jl
@@ -1,3 +1,4 @@
+using Test
 using NaturalSort
 function get_repeat_and_replica_dirs(base_dir)
     # Obtain all directories containing repeats and sort them
@@ -14,6 +15,23 @@ function get_repeat_and_replica_dirs(base_dir)
     end
     return repeat_dirs, dir_dict
 end
+function parse_dS0(file)
+    dS0 = NaN
+    pattern = "[MAIN][0]LLR Delta S"
+    for line in eachline(file)
+        if startswith(line,pattern)
+            dS0 = parse(Float64,line[length(pattern)+1:end])
+            return dS0
+        end
+    end
+    return dS0
+end
 
 base_dir = "/home/fabian/Downloads/llr_parser_test_data/su3_4x20_8/"
 repeats, replica_dirs = get_repeat_and_replica_dirs(base_dir)
+
+fileSU3 = "/home/fabian/Downloads/llr_parser_test_data/su3_4x20_8/0/Rep_0/out_0"
+fileSp4 = "/home/fabian/Downloads/llr_parser_test_data/sp4_4x20_48/0/Rep_0/out_0"
+@test parse_dS0(fileSU3) == 1216.86282
+@test parse_dS0(fileSp4) == 122.55319
+
