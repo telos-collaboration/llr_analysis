aac208755351ac0f08b3332ef91916ec900b2d03
Fabian Zierler <fabian.zierler@a1.net>
2025-03-06 12:16:31 +0000

    write global lattice parameters to file

--- scripts/parse_llr.jl
+++ scripts/parse_llr.jl
@@ -3,10 +3,33 @@ using HDF5
 using LLRParsing
 using BenchmarkTools
 using ProgressMeter
+using HiRepParsing
 
-function parse_entire_llr_dir_to_hdf5(dir,h5file;name=basename(dir)) 
+function _all_files_from_dict(dir,replica_dirs)
+    files = AbstractString[]
+    for repeat in keys(replica_dirs), rep in replica_dirs[repeat]
+        push!(files, joinpath(dir,repeat,rep,"out_0"))
+    end
+    return files
+end
+function parse_entire_llr_dir_to_hdf5(dir,h5file;suffix="") 
     fid = h5open(h5file,"cw")
+
+    # get all repeats and replicas and store that information for future use
     repeats, replica_dirs = get_repeat_and_replica_dirs(dir)
+    files      = _all_files_from_dict(dir,replica_dirs)
+    N_repeats  = length(repeats)
+    # assure the global lattice parameters are identical for all repeats and replicas
+    N_replicas = only(unique([length(replica_dirs[r]) for r in repeats]))
+    Nt = only(unique(first.(latticesize.(files))))  
+    Nl = only(unique(last.(latticesize.(files))))
+
+    name = "$(Nt)x$(Nl)_$(N_repeats)repeats_$(N_replicas)replicas"*suffix
+    write(fid,joinpath(name,"N_repeats"),N_repeats)
+    write(fid,joinpath(name,"N_replicas"),N_replicas)
+    write(fid,joinpath(name,"Nt"),Nt)
+    write(fid,joinpath(name,"Nl"),Nl)
+
     @showprogress desc="parsing $name" for repeat in repeats
         for rep in replica_dirs[repeat]
             file = joinpath(dir, repeat,rep,"out_0")
@@ -35,12 +58,13 @@ function test_parsing(dir)
     end
     
 end
-h5file = "output/Sp4_llr_david.hdf5"
-for dir in readdir("/media/fabian/Adata2TB/sp4_backup_full",join=true)
-    parse_entire_llr_dir_to_hdf5(dir,h5file)
-end
 
-h5file = "output/SU3_llr_david.hdf5"
-for dir in readdir("/media/fabian/Adata2TB/su3_backup_logfiles",join=true)
-    parse_entire_llr_dir_to_hdf5(dir,h5file)
+path = "/home/fabian/Documents/Physics/Data/DataLLR"
+h5fileSp4 = "output/Sp4_llr_david.hdf5"
+h5fileSU3 = "output/SU3_llr_david.hdf5"
+for dir in readdir(joinpath(path,"Sp4/sp4_backup_full"),join=true)
+    parse_entire_llr_dir_to_hdf5(dir,h5fileSp4)
+end
+for dir in readdir(joinpath(path,"SU3/su3_backup_logfiles"),join=true)
+    parse_entire_llr_dir_to_hdf5(dir,h5fileSU3)
 end
