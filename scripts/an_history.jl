24d411064897ff3d120627c77ffe48703556e1ef
Fabian Zierler <fabian.zierler@a1.net>
2025-03-07 15:25:22 +0000

    plot a's against central energy

--- scripts/an_history.jl
+++ scripts/an_history.jl
@@ -1,24 +1,46 @@
 using HDF5
 using Plots
 using Statistics
+gr(frame=:box)
+function a_vs_central_action(h5dset,run)
+    N_replicas = read(h5dset[run],"N_replicas")
+    N_repeats = read(h5dset[run],"N_repeats")
+    a_last = zeros(N_replicas,N_repeats)
+    S_last = zeros(N_replicas,N_repeats)
+    # read all last elements for a and the central action
+    for i in 1:N_replicas, j in 1:N_repeats
+        a_last[i,j] = h5dset[run]["$(j-1)/Rep_$(i-1)/a"][end]
+        S_last[i,j] = h5dset[run]["$(j-1)/Rep_$(i-1)/S0"][end]
+    end
+    # Sort by the central action to account for different swaps
+    # over different repeats
+    for j in 1:N_repeats
+        perm = sortperm(S_last[:,j])
+        S_last[:,j] = S_last[perm,j]
+        a_last[:,j] = a_last[perm,j]
+    end
+    # make sure that the sorted central action alwas matches
+    for i in 1:N_replicas
+        @assert allequal(S_last[i,:])
+    end
+    # average over all repeats
+    S0  = S_last[:,1]
+    a0  = dropdims(mean(a_last,dims=2),dims=2)
+    Δa0 = dropdims(std(a_last,dims=2),dims=2)/sqrt(N_repeats)
+    return a0, Δa0, S0
+end
 
 file = "output/SU3_llr_david.hdf5"
 fid  = h5open(file)
 runs = keys(fid)
-run  = first(runs)
 
-
-N_replicas = read(fid[run],"N_replicas")
-N_repeats = read(fid[run],"N_repeats")
-a_last = zeros(N_replicas,N_repeats)
-p_last = zeros(N_replicas,N_repeats)
-for i in 0:N_replicas-1, j in 0:N_repeats-1
-    a_last[i+1,j+1] = fid[run]["$j/Rep_$i/a"][end]
-    p_last[i+1,j+1] = fid[run]["$j/Rep_$i/plaq"][end]
+plt = plot()
+for run in runs[1:1]
+    a0, Δa0, S0 = a_vs_central_action(fid,run)
+    Nt = read(fid[run],"Nt")
+    Nl = read(fid[run],"Nl")
+    V  = Nl^3 * Nt
+    up = S0/(6V)
+    plot!(plt,up,a0,yerr=Δa0,marker=:auto,label="$(Nt)x$(Nl): ΔE=$(round(2(S0[2]-S0[1])/6V,sigdigits=1))")
 end
-
-a0  = dropdims(mean(a_last,dims=2),dims=2)
-Δa0 = dropdims(std(a_last,dims=2),dims=2)/sqrt(N_repeats)
-p0  = dropdims(mean(p_last,dims=2),dims=2)
-
-scatter(p0,a0,yerr=Δa0)
+plt
