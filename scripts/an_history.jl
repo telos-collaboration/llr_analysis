66fd1c95f2b89d083f4b1aaf518e67965c467eda
Fabian Zierler <fabian.zierler@a1.net>
2025-03-10 16:00:35 +0000

    create hdf5 file that issorted in the central energy (i.e. swaps have compensated in the labelling of trajectories)

--- scripts/an_history.jl
+++ scripts/an_history.jl
@@ -10,19 +10,8 @@ function a_vs_central_action(h5dset,run;ind=nothing)
     # read all last elements for a and the central action
     for i in 1:N_replicas, j in 1:N_repeats
         ind = isnothing(ind) ? length(h5dset[run]["$(j-1)/Rep_$(i-1)/a"]) : ind 
-        a_last[i,j] = h5dset[run]["$(j-1)/Rep_$(i-1)/a"][ind]
-        S_last[i,j] = h5dset[run]["$(j-1)/Rep_$(i-1)/S0"][ind]
-    end
-    # Sort by the central action to account for different swaps
-    # over different repeats
-    for j in 1:N_repeats
-        perm = sortperm(S_last[:,j])
-        S_last[:,j] = S_last[perm,j]
-        a_last[:,j] = a_last[perm,j]
-    end
-    # make sure that the sorted central action alwas matches
-    for i in 1:N_replicas
-        @assert allequal(S_last[i,:])
+        a_last[i,j] = h5dset[run]["$(j-1)/Rep_$(i-1)/a_sorted"][ind]
+        S_last[i,j] = h5dset[run]["$(j-1)/Rep_$(i-1)/S0_sorted"][ind]
     end
     # average over all repeats
     S0  = S_last[:,1]
@@ -31,7 +20,7 @@ function a_vs_central_action(h5dset,run;ind=nothing)
     return a0, Δa0, S0, ind
 end
 
-file = "output/SU3_llr_david.hdf5"
+file = "output/SU3_llr_david_sorted.hdf5"
 fid  = h5open(file)
 runs = keys(fid)
 
@@ -39,6 +28,7 @@ plt = plot()
 for run in runs[1:1]
     for ind in [50,100,300,500]
         a0, Δa0, S0, ind = a_vs_central_action(fid,run;ind)
+
         Nt = read(fid[run],"Nt")
         Nl = read(fid[run],"Nl")
         V  = Nl^3 * Nt
@@ -46,4 +36,4 @@ for run in runs[1:1]
         plot!(plt,up,a0,yerr=Δa0,marker=:auto,label="$(Nt)x$(Nl): ΔE=$(round(2(S0[2]-S0[1])/6V,sigdigits=1)) (Nr+RM steps=$ind)")
     end
 end
-plot!(plt,xlims=(0.547,0.552),ylims=(5.688,5.695))
+plt
