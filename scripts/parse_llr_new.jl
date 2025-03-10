2de9afcf5eec7925571b3d2095e0209f5d7ade3b
Fabian Zierler <fabian.zierler@a1.net>
2025-03-10 22:49:08 +0000

    parse new llr data

--- scripts/parse_llr_new.jl
+++ scripts/parse_llr_new.jl
@@ -1,9 +1,11 @@
 using Pkg; Pkg.activate(".")
 using LLRParsing
 
-path = "/media/fabian/Adata2TB/LLR_New/"
-h5fileSp4 = "output/Sp4_llr_new.hdf5"
+path   = "/media/fabian/Adata2TB/LLR_New/CSD3/"
+h5file = "output/Sp4_llr_new.hdf5"
+h5file_sorted = "output/Sp4_llr_new_sorted.hdf5"
 
 for dir in readdir(path,join=true)
-    llr_dir_hdf5(dir,h5fileSp4)
+    llr_dir_hdf5(dir,h5file)
 end
+sort_by_central_energy_to_hdf5(h5file,h5file_sorted)
