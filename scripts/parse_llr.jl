660353371f668f8023dc070b8c8fe25ac172b4e4
Fabian Zierler <fabian.zierler@a1.net>
2025-03-11 16:00:53 +0000

    combined parsing script

--- scripts/parse_llr.jl
+++ scripts/parse_llr.jl
@@ -1,23 +1,26 @@
 using Pkg; Pkg.activate(".")
 using LLRParsing
 
-#path = "/home/fabian/Documents/Physics/Data/DataLLR"
-#path = "/media/fabian/Adata2TB/LLR/"
-#
-#h5fileSp4 = "output/Sp4_llr_david.hdf5"
-#h5fileSU3 = "output/SU3_llr_david.hdf5"
-#
-#for dir in readdir(joinpath(path,"llr_sp4"),join=true)
-#    llr_dir_hdf5(dir,h5fileSp4)
+path = "/media/fabian/Adata2TB/LLR/"
+path = "/home/fabian/Documents/Physics/Data/DataLLR"
+
+h5fileSp4 = "output/Sp4_llr_david.hdf5"
+h5fileSU3 = "output/SU3_llr_david.hdf5"
+h5fileSp4_new = "output/Sp4_llr_new.hdf5"
+h5fileSp4_sorted = "output/SU3_llr_david_sorted.hdf5"
+h5fileSp4_sorted = "output/SU3_llr_david_sorted.hdf5"
+h5fileSU3_sorted_new = "output/Sp4_llr_new.hdf5"
+
+for dir in readdir(joinpath(path,"llr_sp4_cleaned"),join=true)
+    llr_dir_hdf5(dir,h5fileSp4)
+end
+#for dir in readdir(joinpath(path,"llr_sp4_new_cleaned"),join=true)
+#    llr_dir_hdf5(dir,h5fileSp4_new)
 #end
 #for dir in readdir(joinpath(path,"llr_su3"),join=true)
 #    llr_dir_hdf5(dir,h5fileSU3)
 #end
 
-h5file_in = "output/SU3_llr_david.hdf5"
-h5file_out= "output/SU3_llr_david_sorted.hdf5"
-sort_by_central_energy_to_hdf5(h5file_in,h5file_out)
-
-h5file_in = "output/Sp4_llr_david.hdf5"
-h5file_out= "output/Sp4_llr_david_sorted.hdf5"
-sort_by_central_energy_to_hdf5(h5file_in,h5file_out)
+sort_by_central_energy_to_hdf5(h5fileSp4, h5fileSp4_sorted)
+sort_by_central_energy_to_hdf5(h5fileSU3, h5fileSU3_sorted)
+sort_by_central_energy_to_hdf5(h5fileSp4_new, h5fileSp4_sorted_new)
