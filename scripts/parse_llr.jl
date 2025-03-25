e760dd63d5fbb68846e42f175544b75f43665ff4
Fabian Zierler <fabian.zierler@a1.net>
2025-03-25 10:39:35 +0000

    clean the HiRep output within this directory

--- scripts/parse_llr.jl
+++ scripts/parse_llr.jl
@@ -1,15 +1,14 @@
 using Pkg; Pkg.activate(".")
 using LLRParsing
+using HiRepOutputCleaner
 
-path = "/media/fabian/Adata2TB/LLR/"
-path = "/home/fabian/Documents/Physics/Data/DataLLR"
+path    = "/home/fabian/Documents/Physics/Data/DataCSD/CSD3/"
+newpath = "./output/LLRout" 
+#clean_llr_directory(path,newpath;checkpoint_pattern=nothing,last_ranges=nothing)
+path    = newpath
 
-#h5fileSU3 = "output/SU3_llr_david.hdf5"
-#h5fileSU3_sorted = "output/SU3_llr_david_sorted.hdf5"
-#h5fileSp4 = "output/Sp4_llr_david.hdf5"
-#h5fileSp4_sorted = "output/Sp4_llr_david_sorted.hdf5"
-h5fileSp4_new = "output/Sp4_llr_new.hdf5"
-h5fileSp4_sorted_new = "output/Sp4_llr_new_sorted.hdf5"
+h5fileSp4_new        = "output/test.hdf5"
+h5fileSp4_sorted_new = "output/test_sorted.hdf5"
 
 function llr_alldirs_hdf5(path,base_dir,file)
     for dir in readdir(joinpath(path,base_dir),join=true)
@@ -18,12 +17,6 @@ function llr_alldirs_hdf5(path,base_dir,file)
 end
 
 skip_ens = nothing
-#llr_alldirs_hdf5(path,"llr_su3",h5fileSU3)
-#sort_by_central_energy_to_hdf5(h5fileSU3, h5fileSU3_sorted)
-#llr_alldirs_hdf5(path,"llr_sp4_cleaned",h5fileSp4)
-#sort_by_central_energy_to_hdf5(h5fileSp4, h5fileSp4_sorted)
-#llr_alldirs_hdf5(path,"llr_sp4_new_cleaned",h5fileSp4_new)
+llr_alldirs_hdf5(path,"",h5fileSp4_new)
 isfile(h5fileSp4_sorted_new) && rm(h5fileSp4_sorted_new)
 sort_by_central_energy_to_hdf5(h5fileSp4_new, h5fileSp4_sorted_new; skip_ens)
-
-
