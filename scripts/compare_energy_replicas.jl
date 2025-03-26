4862f1e304875bd7206c82144e32ad79c6dabd1b
Fabian Zierler <fabian.zierler@a1.net>
2025-03-26 15:00:17 +0000

    remove non-matching spurious entries in trajectories by comparing the central energie across all replicas

--- scripts/compare_energy_replicas.jl
+++ scripts/compare_energy_replicas.jl
@@ -2,10 +2,10 @@ using HDF5
 # Utilities for 
 # 1) Finding all indices for unique elements in an array v  
 # 2) Finding the values of all items of v that occur more than once
-# 3) Finding the inidces of all items of v that occur more than once
-nique_indices(v) = unique(i -> v[i], eachindex(v))
+# 3) Finding the inidces of all items of v that occur more than once allowing for slight deviations
+unique_indices(v) = unique(i -> v[i], eachindex(v))
 duplicate_vals(v) = v[setdiff(eachindex(v),unique_indices(v))]
-duplicate_indices(v) = findall(i-> v[i] ∈ duplicate_vals(v), eachindex(v))
+approx_duplicate_indices(v) = findall(i-> any(isapprox(v[i]),duplicate_vals(v)), eachindex(v))
 
 function all_central_enrgies(fid)
     replicas     = keys(fid)
@@ -16,21 +16,22 @@ function all_central_enrgies(fid)
         val = read(fid_repeat,"$rep/S0")
         S0[i,1:length(val)] = val
     end
-    return S0, replicas, traj_lengths
+    return S0
 end
 # Sometimes we have additional data corruption that leads to a non-matching trajectory lengths
 # In order to identify the spurious data, we compare the central energies for every trajectory
 # We then remove duplicates  and/or non-matching central energies from the replica with the longer trajectory
-function find_first_duplicated_central_energies(S0,replicas,traj_lengths)
+function find_first_duplicated_central_energies(S0,traj_lengths)
     Smin,Smax = extrema(filter(isfinite, S0))
-    S0_theory = collect(range(Smin,Smax,length=length(replicas)))
+    nreplicas = first(size(S0))
+    S0_theory = collect(range(Smin,Smax,length=nreplicas))
     traj_min  = minimum(traj_lengths)
     for i in 1:traj_min
         if !isapprox(S0_theory,sort(S0[:,i]))
             # identify duplicate indices and then
             # remove those that correspond to the longer trajectories
             # ans make sure that only duplicates have occured in pairs
-            inds = duplicate_indices(S0[:,i])
+            inds = approx_duplicate_indices(S0[:,i])
             return i, inds
         end
     end
@@ -38,17 +39,28 @@ end
 function remove_central_energy_trajectories!(S0,traj_step,replica_indices)
     for replica_ind in replica_indices
         S0[replica_ind,traj_step:end-1] = S0[replica_ind,traj_step+1:end]
+        # The last entry is then set to infinity, since it corresponds to missing data
+        # and we choose 'Inf' to represent that because it autoimatically places the entry
+        # at the end of the array when sorting it.
+        S0[replica_ind,end] = Inf 
+    end
+    return S0
+end
+function remove_non_matching_trajectories_in_replicas(fid_repeat)
+    S0 = all_central_enrgies(fid_repeat)
+    traj_lengths = dropdims(count(isfinite, S0, dims=2),dims=2)
+
+    ans = find_first_duplicated_central_energies(S0, traj_lengths)
+    while !isnothing(ans)
+        traj_step, indices = ans
+        rm_indices = filter(i -> traj_lengths[i] > minimum(traj_lengths),  indices)
+        S0 = remove_central_energy_trajectories!(S0,traj_step,rm_indices)
+        traj_lengths = dropdims(count(isfinite, S0, dims=2),dims=2)
+        ans = find_first_duplicated_central_energies(S0, traj_lengths)
     end
     return S0
 end
 
 fid = h5open("output/test.hdf5")
 fid_repeat = fid["5x64_19repeats_95replicas/13/"]
-
-S0, replicas, traj_lengths = all_central_enrgies(fid_repeat)
-traj_step, indices = find_first_duplicated_central_energies(S0, replicas, traj_lengths)
-S0 = remove_central_energy_trajectories!(S0,traj_step,indices)
-traj_step, indices = find_first_duplicated_central_energies(S0, replicas, traj_lengths)
-
-Smin,Smax = extrema(filter(isfinite, S0))
-S0_theory = collect(range(Smin,Smax,length=length(replicas)))
+S0 = remove_non_matching_trajectories_in_replicas(fid_repeat)
