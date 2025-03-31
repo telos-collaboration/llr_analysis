b4065d954f07a06ac74dba6f18a7eef588694cca
Fabian Zierler <fabian.zierler@a1.net>
2025-03-31 21:34:21 +0100

    plot different volumes

--- scripts/compare_volumes.jl
+++ scripts/compare_volumes.jl
@@ -6,11 +6,16 @@ gr(fontfamily="Computer Modern",legend=:topleft,frame=:box,titlefontsize=11,lege
 file = "output/test_sorted.hdf5"
 h5id = h5open(file)
 runs = keys(h5id)
-filter!(contains("4x"),runs)
+
+filter!(contains("5x"),runs)
+#runs = runs[[2,4,5,6,8]] # Nt4
+runs = runs[[1,5,6,7]]   # Nt5
 
 plt = plot()
 for run in runs
-    use_lens = run == last(runs)
+    use_lens = false
     LLRParsing.a_vs_central_action_plot!(plt,h5id,run;index=nothing,highlight_index=nothing,lens=use_lens)
 end
-plt
+
+plot!(plt,xlims=(0.569,0.574) ,ylims=(7.333,7.345),legend=:bottomright) # Nt4
+plot!(plt,xlims=(0.5888,0.590),ylims=(7.487,7.492),legend=:bottomright) # Nt5
