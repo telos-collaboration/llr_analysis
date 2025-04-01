23be571a9c32c6603a226498986600a6dec4c8b8
Fabian Zierler <fabian.zierler@a1.net>
2025-04-01 08:57:25 +0100

    plot all Nt at the same time

--- scripts/compare_volumes.jl
+++ scripts/compare_volumes.jl
@@ -3,19 +3,30 @@ using HDF5
 using Plots
 gr(fontfamily="Computer Modern",legend=:topleft,frame=:box,titlefontsize=11,legendfontsize=9,labelfontsize=12,left_margin=0Plots.mm)
 
+a_vs_central_action_plot(h5id,runs::Vector;kws...) = a_vs_central_action_plot!(plot(),h5id,runs;kws...)
+function a_vs_central_action_plot!(plt,h5id,runs::Vector;kws...)
+    for run in runs
+        LLRParsing.a_vs_central_action_plot!(plt,h5id,run;kws...)
+    end
+    return plt
+end     
+
 file = "output/test_sorted.hdf5"
 h5id = h5open(file)
 runs = keys(h5id)
 
-filter!(contains("5x"),runs)
-#runs = runs[[2,4,5,6,8]] # Nt4
-runs = runs[[1,5,6,7]]   # Nt5
+runsNt4 = filter(contains("4x"),runs)
+runsNt5 = filter(contains("5x"),runs)
+runsNt6 = filter(contains("6x"),runs)
 
-plt = plot()
-for run in runs
-    use_lens = false
-    LLRParsing.a_vs_central_action_plot!(plt,h5id,run;index=nothing,highlight_index=nothing,lens=use_lens)
-end
+pltNt4 = a_vs_central_action_plot(h5id,runsNt4,lens=false)
+pltNt5 = a_vs_central_action_plot(h5id,runsNt5,lens=false)
+pltNt6 = a_vs_central_action_plot(h5id,runsNt6,lens=false)
 
-plot!(plt,xlims=(0.569,0.574) ,ylims=(7.333,7.345),legend=:bottomright) # Nt4
-plot!(plt,xlims=(0.5888,0.590),ylims=(7.487,7.492),legend=:bottomright) # Nt5
+plot!(pltNt4,xlims=(0.569,0.574) ,ylims=(7.333,7.345),legend=:bottomright) # Nt4
+plot!(pltNt5,xlims=(0.5888,0.590),ylims=(7.487,7.492),legend=:bottomright) # Nt5
+plot!(pltNt6,legend=:bottomright) # pltNt6
+
+display(pltNt4)
+display(pltNt5)
+display(pltNt6)
