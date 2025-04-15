b2ccf02e4279e2afb50e0251442f9ec3f87817e5
Fabian Zierler <fabian.zierler@a1.net>
2025-04-15 14:54:43 +0100

    compare and plot only Nt=5

---
+++ scripts/compare_volumesNt5.jl
@@ -0,0 +1,22 @@
+using LLRParsing
+using HDF5
+using Plots
+gr(fontfamily="Computer Modern",legend=:topleft,frame=:box,titlefontsize=11,legendfontsize=9,labelfontsize=12,left_margin=0Plots.mm)
+
+a_vs_central_action_plot(h5id,runs::Vector;kws...) = a_vs_central_action_plot!(plot(),h5id,runs;kws...)
+function a_vs_central_action_plot!(plt,h5id,runs::Vector;kws...)
+    for run in runs
+        LLRParsing.a_vs_central_action_plot!(plt,h5id,run;kws...)
+    end
+    return plt
+end     
+
+isdir("plots") || mkdir("plots")
+file = "output/test_sorted.hdf5"
+h5id = h5open(file)
+runs = keys(h5id)
+
+runsNt5 = filter(contains("5x"),runs)
+pltNt5 = a_vs_central_action_plot(h5id,runsNt5,lens=false)
+plot!(pltNt5,xlims=(0.5888,0.590),ylims=(7.487,7.492),legend=:bottomright) # Nt5
+savefig(pltNt5,joinpath("plots","Nt5.pdf"))
