64acdbb724f7138c9e002a3015e1add1d16e142d
Fabian Zierler <fabian.zierler@a1.net>
2025-04-29 16:10:24 +0100

    probability distribution for one repeat

--- scripts/plaquette_histogram.jl
+++ scripts/plaquette_histogram.jl
@@ -1,6 +1,6 @@
 using HDF5
 using LLRParsing
-
+using Plots
 # In python, we are using the following precision
 # Mpmath settings:
 #   mp.prec = 53                [default: 53]
@@ -28,32 +28,6 @@ function log_partition_function(a, S, beta)
     end
     return Float64(log(2Z))
 end
-
-file = "output/SU3_llr_sorted.hdf5"
-fid  = h5open(file)
-
-run     = "4x20_20repeats_55replicas"
-repeat  = "0"
-replica = "Rep_0"
-
-# I have checked that those are the correct values that David's code is using
-a_all, S_all = LLRParsing.a_vs_central_action_repeats(fid,run;ind=nothing)[1:2]
-
-Nl = read(fid[run],"Nl")
-Nt = read(fid[run],"Nt")
-V  = Nl^3 * Nt
-
-a    = a_all[:,end]
-S    = S_all[:,end]
-beta = 5.68
-logZ = log_partition_function(a, S, beta)
-
-up   = S/(6V)
-dS   = S[2] - S[1]
-δup  = dS/(6V)
-ups  = range(minimum(up) + δup, maximum(up), length=1000)
-E    = @. ups*6V
-
 function log_rho(E, S, dS, a)
     # David uses a different sign for a
     a = -a
@@ -71,9 +45,36 @@ function log_rho(E, S, dS, a)
     @assert !iszero(log_ρ)
     return Float64(log_ρ)
 end
+function probability_density(a, S, beta, V)
+    up   = S/(6V)
+    dS   = S[2] - S[1]
+    δup  = dS/(6V)
+    ups  = range(minimum(up) + δup, maximum(up), length=1000)
+    E    = @. ups*6V
 
-log_ρ = log_rho.(E, Ref(S), dS, Ref(a))
-probability_density = @. exp(log_ρ + beta*ups*V*6 - logZ)
+    logZ = log_partition_function(a, S, beta)
+    log_ρ = log_rho.(E, Ref(S), dS, Ref(a))
+    probability_density = @. exp(log_ρ + beta*ups*V*6 - logZ)
+    return ups, probability_density
+end
 
+file = "output/SU3_llr_sorted.hdf5"
+fid  = h5open(file)
 
-histogram(probability_density)
+keys(fid)
+
+run     = "4x20_20repeats_108replicas"
+repeat  = "0"
+replica = "Rep_0"
+
+# I have checked that those are the correct values that David's code is using
+a_all, S_all = LLRParsing.a_vs_central_action_repeats(fid,run;ind=nothing)[1:2]
+Nl   = read(fid[run],"Nl")
+Nt   = read(fid[run],"Nt")
+V    = Nl^3 * Nt
+a    = a_all[:,end]
+S    = S_all[:,end]
+beta = 5.69193
+
+ups,prob = probability_density(a, S, beta, V)
+plot(ups,prob)
