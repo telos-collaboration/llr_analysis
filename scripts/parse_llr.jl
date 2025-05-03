bafe945dd50834fe5aada38d41f70b107fb218e1
Fabian Zierler <fabian.zierler@a1.net>
2025-05-03 13:22:32 +0100

    test continuation

--- scripts/parse_llr.jl
+++ scripts/parse_llr.jl
@@ -4,13 +4,14 @@ using HiRepOutputCleaner
 
 path = "/home/fabian/Documents/Physics/Data/DataCSD/CSD3/"
 path = "/home/fabian/Documents/Physics/Data/DataLLR/llr_sp4_cleaned"
-newpath = "./output/LLRout"
+path = "/home/fabian/Downloads/LLR_cont"
+#newpath = "./output/LLRout"
 #clean_llr_directory(path,newpath;checkpoint_pattern=nothing,last_ranges=nothing,warn=false)
 #path    = newpath
 
 isdir("./output") || mkdir("./output")
-h5file        = "output/test.hdf5"
-h5file_sorted = "output/test_sorted.hdf5"
+h5file        = "output/test_Nt5.hdf5"
+h5file_sorted = "output/test_Nt5_sorted.hdf5"
 
 function llr_alldirs_hdf5(path,base_dir,file)
     for dir in readdir(joinpath(path,base_dir),join=true)
@@ -25,5 +26,5 @@ isfile(h5file) && rm(h5file)
 isfile(h5file_sorted) && rm(h5file_sorted)
 llr_alldirs_hdf5(path,"",h5file)
 sort_by_central_energy_to_hdf5(h5file, h5file_sorted)
-include("trajectory_overview.jl")
-include("compare_volumesNt5.jl")
+#include("trajectory_overview.jl")
+#include("compare_volumesNt5.jl")
