63ceb6a2f19518b7776a0bf921d0482ffaf242fc
Fabian Zierler <fabian.zierler@a1.net>
2025-05-06 15:57:06 +0100

    clean data on case-by-case basis

--- scripts/parse_llr.jl
+++ scripts/parse_llr.jl
@@ -3,23 +3,28 @@ using LLRParsing
 using HiRepOutputCleaner
 using DelimitedFiles
 
-path = "/home/fabian/Documents/Physics/Data/" 
-dirs = readdlm("metadata/runs.csv",',',skipstart=1)
-
+path          = "/home/fabian/Documents/Physics/Data/" 
+metadata      = "metadata/runs.csv"
 h5file        = "data_assets/test_Nt5.hdf5"
 h5file_sorted = "data_assets/test_Nt5_sorted.hdf5"
 
-function llr_alldirs_hdf5(path,dirs,h5file)
-    for dir in joinpath.(Ref(path),dirs)
-        tmpdir = "./tmp/$(basename(dir))/"
-        ispath(tmpdir) || mkpath(tmpdir)
-        clean_llr_directory(dir,tmpdir;checkpoint_pattern=nothing,last_ranges=nothing,warn=false)
-        llr_dir_hdf5(tmpdir,h5file)
-        rm(tmpdir,recursive=true)
+function llr_alldirs_hdf5(path,metadata_file,h5file)
+    metadata = readdlm(metadata_file,',',skipstart=1)
+    for (run,clean) in eachrow(metadata)
+        dir = joinpath(path,run)
+        if clean
+            tmpdir = "./tmp/$(basename(dir))/"
+            ispath(tmpdir) || mkpath(tmpdir)
+            clean_llr_directory(dir,tmpdir;checkpoint_pattern=nothing,last_ranges=nothing,warn=false)
+            llr_dir_hdf5(tmpdir,h5file)
+            rm(tmpdir,recursive=true)
+        else
+            llr_dir_hdf5(dir,h5file)
+        end
     end
 end
 
 isfile(h5file) && rm(h5file)
 isfile(h5file_sorted) && rm(h5file_sorted)
-llr_alldirs_hdf5(path,dirs,h5file)
+llr_alldirs_hdf5(path,metadata,h5file)
 sort_by_central_energy_to_hdf5(h5file, h5file_sorted)
