5e6b6e13366d87798557887cb54e6ad12936e2a6
Fabian Zierler <fabian.zierler@a1.net>
2025-06-02 20:58:19 +0100

    restructure code for fitting double gaussian

--- scripts/fit_plaquette_histogram.jl
+++ scripts/fit_plaquette_histogram.jl
@@ -3,7 +3,27 @@ using LLRParsing
 using Plots
 using Statistics
 using LaTeXStrings
+using Peaks
+using LsqFit
 gr(fontfamily="Computer Modern",legend=:topright,frame=:box,titlefontsize=11,legendfontsize=9,labelfontsize=12,left_margin=0Plots.mm)
+
+Gaussian(x,A,μ,σ) = A * exp(-(x-μ)^2 / σ^2 / 2)
+DoubleGaussian(x,A1,μ1,σ1,A2,μ2,σ2) = Gaussian(x,A1,μ1,σ1) + Gaussian(x,A2,μ2,σ2)
+@. modelDG(x, p) = DoubleGaussian(x,p[1],p[2],p[3],p[4],p[5],p[6])
+function initial_param_double_gaussian(ups,P)
+    δups = ups[2]-ups[1]
+    pks  = findmaxima(vec(P),50)
+    pks  = peakproms(pks)
+    pks  = peakwidths(pks)
+    p0   = [ pks.heights[1],ups[pks.indices[1]],pks.widths[1]*δups,
+    pks.heights[2],ups[pks.indices[2]],pks.widths[2]*δups ]
+    return p0
+end
+function fit_double_gaussian(ups,P)
+    p0  = initial_param_double_gaussian(ups,P)
+    fit = curve_fit(modelDG, ups, vec(P), p0)
+    return fit 
+end
 
 file  = "data_assets/test_Nt5_sorted.hdf5"
 fid   = h5open(file)
@@ -13,22 +33,8 @@ ind   = 5
 beta  = betas[ind]
 run   = runs[ind] 
 
-xl   = (0.5885,0.5905)
 ups,P,ΔP,V,dS = probability_density(fid, run, beta)
-plot_plaquette_histogram!(plt,fid,run,betas[i];xlims=xl)
-
-using Peaks
-δups = ups[2]-ups[1]
-pks  = findmaxima(vec(P),50)
-pks  = peakproms(pks)
-pks  = peakwidths(pks)
-
-using LsqFit
-Gaussian(x,A,μ,σ) = A * exp(-(x-μ)^2 / σ^2 / 2)
-DoubleGaussian(x,A1,μ1,σ1,A2,μ2,σ2) = Gaussian(x,A1,μ1,σ1) + Gaussian(x,A2,μ2,σ2)
-@. model(x, p) = DoubleGaussian(x,p[1],p[2],p[3],p[4],p[5],p[6])
-p0  = [pks.heights[1],ups[pks.indices[1]],pks.widths[1]*δups,pks.heights[2],ups[pks.indices[2]],pks.widths[2]*δups]
-fit = curve_fit(model, ups, vec(P), p0)
-y0  = model(ups,p0)
-y   = model(ups,fit.param)
-plot!(plt,ups,y,label="double Gaussian fit",lw=3)
+fit = fit_double_gaussian(ups,P)
+plt = plot_plaquette_histogram!(plot(),fid,run,betas[ind];xlims=(0.5885,0.5905))
+y   = modelDG(ups,fit.param)
+plot!(plt,ups,y*6V,label="double Gaussian fit",lw=3)
