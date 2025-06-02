6a4d103ab7e7363ded216ed672e8da5329015ba7
Fabian Zierler <fabian.zierler@a1.net>
2025-06-02 13:51:28 +0100

    function barriers for trajectory overview plot

--- scripts/trajectory_overview.jl
+++ scripts/trajectory_overview.jl
@@ -5,19 +5,18 @@ gr(fontfamily="Computer Modern",legend=:topright,frame=:box,titlefontsize=11,leg
 
 h5file_out = "data_assets/test_Nt5_sorted.hdf5"
 h5dset = h5open(h5file_out)
-runs = keys(h5dset)
+runs = [last(keys(h5dset))]
 
-for run in runs
-    @show run
-    # Plot 
-    Nreplicas        = read(h5dset[run],"N_replicas")
-    a0, Δa0, S0, ind = a_vs_central_action(h5dset,run)
-    replica_id       = Nreplicas÷2
-    repeat_id        = 1
+function overview(h5dset,run;repeat_id=1, replica_id = read(h5dset[run],"N_replicas")÷2)
+    Nreplicas  = read(h5dset[run],"N_replicas")
     plt1 = full_trajectory_plot(h5dset,run,repeat_id,replica_id ,lens=false)
     plt2 = full_trajectory_plot(h5dset,run,repeat_id,Nreplicas-1,lens=false)
     plt3 = full_trajectory_plot(h5dset,run,repeat_id,1          ,lens=false)
     plot!(plt2,legend=:topleft)
     plt = plot(plt3, plt1, plt2, layout = grid(1, 3), size=(1400,1000))
-    display(plt)
+    return plt
+end
+for run in runs
+   plt = overview(h5dset,run)
+   display(plt)
 end
