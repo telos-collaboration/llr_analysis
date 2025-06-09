e729cea3cdcf43fc6bc8882d708f2cf35766e90d
Fabian Zierler <fabian.zierler@a1.net>
2025-06-09 14:59:39 +0100

    plot correlation in a_n and P_beta

---
+++ scripts/correlated_an.jl
@@ -0,0 +1,35 @@
+using LLRParsing
+using HDF5
+using Statistics
+using Plots
+using LaTeXStrings
+gr(fontfamily="Computer Modern",legend=:topleft,frame=:box,titlefontsize=11,legendfontsize=9,labelfontsize=12,left_margin=0Plots.mm)
+
+file  = "data_assets/test_Nt5_sorted.hdf5"
+fid   = h5open(file)
+typeof(fid)
+runs  = keys(fid)
+run   = runs[end-2]
+
+a, S, Nt, Nl, V = LLRParsing._set_up_histogram(fid,run)
+beta            = LLRParsing.beta_at_equal_heights(fid,run)
+ups,prob,V,dS   = LLRParsing.probability_density_repeats(a, S, beta, V)
+corP            = cor(prob,dims=2)
+cora            = cor(a,dims=2)
+
+function plot_pearson_correlation_matrix(cor;title)
+    ylabel = "replica number"
+    xlabel = "replica number"
+    plt    = plot(;title, xlabel, ylabel, yflip=true)
+    heatmap!(plt, abs.(cor))
+    return plt
+end
+
+title_a  = "Pearson correlation matrix of \$a_n:\$"*LLRParsing.fancy_title(run)
+title_a  = replace(title_a,"\$\$"=>"")
+title_P  = "Pearson correlation matrix of \$P_\\beta:\$"*LLRParsing.fancy_title(run)
+title_P  = replace(title_P,"\$\$"=>"")
+title_P
+
+plot_pearson_correlation_matrix(cora; title = title_a)
+plot_pearson_correlation_matrix(corP; title = title_P)
