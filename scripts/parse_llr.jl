a45a86f4eaa81b795484ca1cb7fc3c6d8e575577
Fabian Zierler <fabian.zierler@a1.net>
2025-06-11 13:12:07 +0100

    restructure main parsing script

--- scripts/parse_llr.jl
+++ scripts/parse_llr.jl
@@ -3,18 +3,24 @@ using LLRParsing
 using HiRepOutputCleaner
 using DelimitedFiles
 
-function llr_alldirs_hdf5(path,metadata_file,h5file,cleaned=true)
+function clean_all_llr_dirs(path,metadata_file;target="./tmp/")
     metadata = readdlm(metadata_file,',',skipstart=1)
-    tmpdir   = "./tmp/"
-    if !cleaned
-        for run in eachrow(String.(metadata))
-            dir = joinpath(path,run[1])
-            p   = joinpath(tmpdir,basename(dir))
-            ispath(p) || mkpath(p)
-            clean_llr_directory(dir,p;checkpoint_pattern=nothing,last_ranges=nothing,warn=false)
-        end
+    for run in eachrow(String.(metadata))
+        dir = joinpath(path,run[1])
+        p   = joinpath(target,basename(dir))
+        ispath(p) || mkpath(p)
+        clean_llr_directory(dir,p;checkpoint_pattern=nothing,last_ranges=nothing,warn=false)
     end
-    for dir in readdir(tmpdir,join=true)
+end
+function llr_alldirs_hdf5(path,metadata_file,h5file;clean=false)
+    metadata = readdlm(metadata_file,',',skipstart=1)
+    runs     = joinpath.(path,String.(metadata))
+    if clean
+        tmpdir = "./tmp/"
+        clean_all_llr_dirs(path,metadata_file;target=tmpdir)
+        runs = readdir(tmpdir,join=true)
+    end
+    for dir in runs
         llr_dir_hdf5(dir,h5file)
     end
 end
@@ -27,5 +33,16 @@ h5file_sorted = "data_assets/Sp4_Nt5_sorted.hdf5"
 
 isfile(h5file) && rm(h5file)
 isfile(h5file_sorted) && rm(h5file_sorted)
-llr_alldirs_hdf5(path,metadata,h5file)
+llr_alldirs_hdf5(path,metadata,h5file; clean=true)
 sort_by_central_energy_to_hdf5(h5file, h5file_sorted)
+
+# Nt=4
+#path          = "/home/fabian/Documents/Physics/Data/" 
+#metadata      = "metadata/runsNt4.csv"
+#h5file        = "data_assets/Sp4_Nt4.hdf5"
+#h5file_sorted = "data_assets/Sp4_Nt4_sorted.hdf5"
+#
+#isfile(h5file) && rm(h5file)
+#isfile(h5file_sorted) && rm(h5file_sorted)
+#llr_alldirs_hdf5(path,metadata,h5file; clean=false)
+#sort_by_central_energy_to_hdf5(h5file, h5file_sorted)
