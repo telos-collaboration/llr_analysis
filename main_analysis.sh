d8320d274410490430cab317f02b4cfcbdcb9fd9
Fabian Zierler <fabian.zierler@a1.net>
2025-06-16 21:07:39 +0100

    move scripts that are completed for now

--- main_analysis.sh
+++ main_analysis.sh
@@ -1,5 +1,4 @@
-julia scripts/trajectory_overview.jl --h5file "data_assets/Sp4_Nt5_sorted.hdf5" --plot_dir "data_assets/overview_Nt5"
-julia scripts/trajectory_overview.jl --h5file "data_assets/Sp4_Nt4_sorted.hdf5" --plot_dir "data_assets/overview_Nt4"
-
-julia scripts/compare_volumes.jl --h5file "data_assets/Sp4_Nt5_sorted.hdf5" --plot_file "assets/an_volume_Nt5.pdf" --title "\$a_n\$ volume dependence for \$N_t=5\$" --xmin 0.5889 --xmax 0.5898 --ymin 7.488 --ymax 7.491
-julia scripts/compare_volumes.jl --h5file "data_assets/Sp4_Nt4_sorted.hdf5" --plot_file "assets/an_volume_Nt4.pdf" --title "\$a_n\$ volume dependence for \$N_t=4\$" --xmin 0.5695 --xmax 0.573  --ymin 7.337 --ymax 7.343
+julia scripts/done/trajectory_overview.jl --h5file "data_assets/Sp4_Nt5_sorted.hdf5" --plot_dir "data_assets/overview_Nt5"
+julia scripts/done/trajectory_overview.jl --h5file "data_assets/Sp4_Nt4_sorted.hdf5" --plot_dir "data_assets/overview_Nt4"
+julia scripts/done/compare_volumes.jl --h5file "data_assets/Sp4_Nt5_sorted.hdf5" --plot_file "assets/an_volume_Nt5.pdf" --title "\$a_n\$ volume dependence for \$N_t=5\$" --xmin 0.5889 --xmax 0.5898 --ymin 7.488 --ymax 7.491
+julia scripts/done/compare_volumes.jl --h5file "data_assets/Sp4_Nt4_sorted.hdf5" --plot_file "assets/an_volume_Nt4.pdf" --title "\$a_n\$ volume dependence for \$N_t=4\$" --xmin 0.5695 --xmax 0.573  --ymin 7.337 --ymax 7.343
--- main_parsing.sh
+++ main_parsing.sh
@@ -1,6 +1,6 @@
 path="/home/fabian/Documents/Physics/Data/"
 path="/home/fabian/Dokumente/Physics/Data/"
 # Nt=5
-julia scripts/parse_llr.jl --path $path --metadata "metadata/runs.csv" --h5file_unsorted "data_assets/Sp4_Nt5.hdf5" --h5file "data_assets/Sp4_Nt5_sorted.hdf5" --clean true
+julia scripts/done/parse_llr.jl --path $path --metadata "metadata/runs.csv" --h5file_unsorted "data_assets/Sp4_Nt5.hdf5" --h5file "data_assets/Sp4_Nt5_sorted.hdf5" --clean true
 # Nt=4
-julia scripts/parse_llr.jl --path $path --metadata "metadata/runsNt4.csv" --h5file_unsorted "data_assets/Sp4_Nt4.hdf5" --h5file "data_assets/Sp4_Nt4_sorted.hdf5" --clean false
+julia scripts/done/parse_llr.jl --path $path --metadata "metadata/runsNt4.csv" --h5file_unsorted "data_assets/Sp4_Nt4.hdf5" --h5file "data_assets/Sp4_Nt4_sorted.hdf5" --clean false
