4bce79a0b74191a70890355d44b79a27cae7852e
Fabian Zierler <fabian.zierler@a1.net>
2025-06-17 11:53:05 +0100

    combine main scripts, add timing information.

--- main.sh
+++ main.sh
@@ -1,5 +1,11 @@
+path="."
 julia scripts/instantiate.jl
 
+# parsing
+julia scripts/done/parse_llr.jl --path $path --metadata "metadata/runsNt5.csv" --h5file_unsorted "data_assets/Sp4_Nt5.hdf5" --h5file "data_assets/Sp4_Nt5_sorted.hdf5" --clean false
+julia scripts/done/parse_llr.jl --path $path --metadata "metadata/runsNt4.csv" --h5file_unsorted "data_assets/Sp4_Nt4.hdf5" --h5file "data_assets/Sp4_Nt4_sorted.hdf5" --clean false
+
+# analysis
 julia scripts/done/trajectory_overview.jl --h5file "data_assets/Sp4_Nt5_sorted.hdf5" --plot_dir "assets/overview_Nt5"
 julia scripts/done/trajectory_overview.jl --h5file "data_assets/Sp4_Nt4_sorted.hdf5" --plot_dir "assets/overview_Nt4"
 julia scripts/done/compare_volumes.jl --h5file "data_assets/Sp4_Nt5_sorted.hdf5" --plot_file "assets/an_volume_Nt5.pdf" --title "\$a_n\$ volume dependence for \$N_t=5\$" --xmin 0.5889 --xmax 0.5898 --ymin 7.488 --ymax 7.491
