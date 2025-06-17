3b257c4cf7ceaa84a30371a5936e4929ca4e9e48
Fabian Zierler <fabian.zierler@a1.net>
2025-06-17 11:37:38 +0100

    instantiate julia environment at the start of both main scripts

--- main_analysis.sh
+++ main_analysis.sh
@@ -1,3 +1,5 @@
+julia scripts/instantiate.jl
+
 julia scripts/done/trajectory_overview.jl --h5file "data_assets/Sp4_Nt5_sorted.hdf5" --plot_dir "assets/overview_Nt5"
 julia scripts/done/trajectory_overview.jl --h5file "data_assets/Sp4_Nt4_sorted.hdf5" --plot_dir "assets/overview_Nt4"
 julia scripts/done/compare_volumes.jl --h5file "data_assets/Sp4_Nt5_sorted.hdf5" --plot_file "assets/an_volume_Nt5.pdf" --title "\$a_n\$ volume dependence for \$N_t=5\$" --xmin 0.5889 --xmax 0.5898 --ymin 7.488 --ymax 7.491
--- main_parsing.sh
+++ main_parsing.sh
@@ -1,4 +1,5 @@
 path="."
+julia scripts/instantiate.jl
 
 julia scripts/done/parse_llr.jl --path $path --metadata "metadata/runsNt5.csv" --h5file_unsorted "data_assets/Sp4_Nt5.hdf5" --h5file "data_assets/Sp4_Nt5_sorted.hdf5" --clean false
 julia scripts/done/parse_llr.jl --path $path --metadata "metadata/runsNt4.csv" --h5file_unsorted "data_assets/Sp4_Nt4.hdf5" --h5file "data_assets/Sp4_Nt4_sorted.hdf5" --clean false
