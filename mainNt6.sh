a2df93186e278b7e2d2144b84b27c2d87981392d
Fabian Zierler <fabian.zierler@a1.net>
2025-06-23 11:59:01 +0100

    include Nt=6 in the default main file

--- mainNt6.sh
+++
@@ -1,5 +0,0 @@
-path="."
-julia scripts/instantiate.jl
-# Nt6
-julia scripts/done/parse_llr.jl --path $path --metadata "metadata/runsNt6.csv" --h5file_unsorted "data_assets/Sp4_Nt6.hdf5" --h5file "data_assets/Sp4_Nt6_sorted.hdf5" --clean false
-julia scripts/done/compare_volumes.jl --h5file "data_assets/Sp4_Nt6_sorted.hdf5" --plot_file "assets/an_volume_Nt6.pdf" --title "\$a_n\$ volume dependence for \$N_t=6\$" --xmin 0.6015 --xmax 0.6025 --ymin 7.615 --ymax 7.623
