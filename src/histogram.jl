3769e551f777139d7f5f8b1302538b18647247b4
Fabian Zierler <fabian.zierler@a1.net>
2025-07-07 18:06:39 +0100

    move code for calculation of plaquette moments into library

--- src/histogram.jl
+++ src/histogram.jl
@@ -5,20 +5,65 @@
 #   mp.trap_complex = False     [default: False]
 setprecision(BigFloat, 53)
 
-function log_partition_function(a, S, beta)
+function log_partition_function(a, S, beta, ::Type{T}=BigFloat) where T
     # David uses a different sign for a
     dS     = S[2] - S[1]    
-    pi_exp = 0.0
-    Z      = BigFloat(0)
+    pi_exp = T(0)
+    Z      = T(0)
     for (ai, Si) in zip(a,S)
-        A = beta - ai
-        exp_factor  = exp(BigFloat(pi_exp + Si*beta - ai*dS/2))
+        A           = beta - ai
+        exp_factor  = exp(T(pi_exp + Si*beta - ai*dS/2))
         sinh_factor = iszero(A) ? dS/2 : sinh(A*dS/2)/A
         Z      += exp_factor*sinh_factor
         pi_exp -= ai*dS
     end
-    return Float64(log(2Z))
+    return log(2Z)
 end
+
+"""
+    energy_moment(S,a,β,N,[::Type{T}=Float64,::Type{U}=BigFloat])
+
+    Calculate N-th cumulant of the energy from the d.o.s. coefficients `a`
+    at the central energies `S` at the inverse coupling β.
+
+    This code has been ported over from David Mason's implementation
+    in python (10.5281/zenodo.13807993). It follows Eqs. (3.1.18) and
+    (3.1.19) from David Masons's PhD thesis. 
+
+    By default, we use julia's BigFloat together with the double precision Float64 
+    datatype for floating point calculations. This can be overwritten by specifying
+    the datatypes `T` and `U` to be used instead.
+
+    Typically, it suffices to use `T=Float64` for all calculations in here as long 
+    as log(Z) is calculated in higher precision e.g. `U=BigFloat`.
+"""
+function energy_moment(S,a,β,N::Int,::Type{T}=Float64,::Type{U}=BigFloat) where {T,U}
+    pi_exp = - T(log_partition_function(a, S, β, U))
+    full_exp = T(0)
+    En = T(0)
+    δS = S[2] - S[1]
+    for (Si,ai) in zip(S,a)
+        A = T(- ai + β)
+        full_exp = exp(pi_exp + β*(Si-δS/2) + A*δS/2 ) 
+        for m in 0:N
+            sinh_term = T(0)
+            cosh_term = T(0)
+            for j in 0:div(m,2,RoundDown)
+                sinh_term += (δS/2)^(2j)*Si^(m-2j)/factorial(2j)/factorial(m-2j)
+            end
+            for j in 1:div(m,2,RoundUp)
+                cosh_term += (δS/2)^(2j-1)*Si^(m-2j+1)/factorial(2j-1)/factorial(m-2j+1)
+            end
+            sh = sinh(A*δS/2)
+            ch = cosh(A*δS/2)
+            Ap = A^(m-N-1)
+            En += 2*factorial(N)*full_exp*(sh*sinh_term + ch*cosh_term)*Ap*(-1)^(N-m)
+        end   
+        pi_exp -= ai*δS
+    end
+    return En
+end
+
 _E_in_interval(E,S0,dS) = E >= (S0 - dS/2) && E < (S0 + dS/2)
 function log_rho(E, S, dS, a; cumsum_a = cumsum(a))
     for i in eachindex(S, a)
@@ -50,7 +95,7 @@ function trapz(x,y)
     end
     return s 
 end
-function probability_density_repeats(a, S, beta, V; nbins=length(S))
+function probability_density_repeats(a, S, beta, V; nbins=length(S), normalize=false)
     dS  = S[2] - S[1]
     E   = range(minimum(S) + dS, maximum(S), length=nbins)
     ups = @. E/(6V) 
@@ -58,15 +103,17 @@ function probability_density_repeats(a, S, beta, V; nbins=length(S))
     P = zeros(nbins,repeats)
 
     for i in 1:repeats
-        logZ = log_partition_function(a[:,i], S, beta)
+        logZ = Float64(log_partition_function(a[:,i], S, beta))
         csa  = cumsum(a[:,i])
         for j in 1:nbins
             log_ρ  = log_rho(E[j], S, dS, a[:,i]; cumsum_a=csa)
             P[j,i] = exp(log_ρ + beta*E[j] - logZ)
         end
-        # not really needed. ∫PdE is already approx. 1
-        # intP = trapz(E,P[:,i])
-        # @. P[:,i] = P[:,i] / intP
+        if normalize
+            #not really needed. ∫PdE is already approx. 1
+            intP = trapz(E,P[:,i])
+            @. P[:,i] = P[:,i] / intP
+        end
     end
     return ups, P, V, dS
 end
