9737d0a850d2b8fd37f80b40a6ca48a5d49902e2
Fabian Zierler <fabian.zierler@a1.net>
2025-07-07 16:42:44 +0100

    isolate expensive calls in energy momenta calculation

--- scripts/devel/cumulants.jl
+++ scripts/devel/cumulants.jl
@@ -27,16 +27,14 @@ end
     in python (10.5281/zenodo.13807993). It follows Eqs. (3.1.18) and
     (3.1.19) from David Masons's PhD thesis. 
 """
-function energy_cumulant(S,a,β,N::Int)
+function energy_cumulant(S,a,β::Number,N::Int)
     pi_exp = - log_partition_function(a, S, β)
     full_exp = BigFloat(0)
     En = BigFloat(0)
     δS = S[2] - S[1]
     for (Si,ai) in zip(S,a)
-        A = - BigFloat(ai) + BigFloat(β)
-        
+        A = BigFloat( - ai + β)
         full_exp = 2*factorial(N)*exp(pi_exp + β*(Si-δS/2) + A*δS/2 ) 
-        T = A*δS/2
         for m in 0:N
             sinh_term = BigFloat(0)
             cosh_term = BigFloat(0)
@@ -46,9 +44,11 @@ function energy_cumulant(S,a,β,N::Int)
             for j in 1:div(m,2,RoundUp)
                 cosh_term += (δS/2)^(2j-1)*Si^(m-2j+1)/factorial(2j-1)/factorial(m-2j+1)
             end
-            En += full_exp*(sinh(T)*sinh_term + cosh(T)*cosh_term)*A^(m-N-1)*(-1)^(N-m)
-        end
-        
+            sh = sinh(A*δS/2)
+            ch = cosh(A*δS/2)
+            Ap = A^(m-N-1)
+            En += full_exp*(sh*sinh_term + ch*cosh_term)*Ap*(-1)^(N-m)
+        end   
         pi_exp -= ai*δS
     end
     return En
@@ -64,6 +64,8 @@ end
 
 using Plots
 using HDF5
+using BenchmarkTools
+using Profile
 
 h5file = "data_assets/Sp4_Nt4_sorted.hdf5"
 fid = h5open(h5file) 
@@ -71,12 +73,18 @@ r = keys(fid)[1]
 
 a, S, Nt, Nl, V = _set_up_histogram(fid,r)
 mina, maxa = extrema(a)
-βs = range(start=mina,stop=maxa,length=100)
+βs = collect(range(start=mina,stop=maxa,length=1000))
+    
+energy_cumulant.(Ref(S),Ref(0),βs,4)
+Profile.init()
+@profview energy_cumulant.(Ref(S),Ref(0),βs,4)
+@time energy_cumulant.(Ref(S),Ref(0),βs,4)
+@btime energy_cumulant.(Ref(S),Ref(0),βs,4)
 
-E0 = [ energy_cumulant(S,a,β,0)/(6V)^0 for β in βs ]
-E1 = [ energy_cumulant(S,a,β,1)/(6V)^1 for β in βs ]
-E2 = [ energy_cumulant(S,a,β,2)/(6V)^2 for β in βs ]
-E4 = [ energy_cumulant(S,a,β,4)/(6V)^4 for β in βs ]
+E0 = energy_cumulant.(Ref(S),Ref(a),βs,0)/(6V)^0;
+E1 = energy_cumulant.(Ref(S),Ref(a),βs,1)/(6V)^1;
+E2 = energy_cumulant.(Ref(S),Ref(a),βs,2)/(6V)^2;
+E4 = energy_cumulant.(Ref(S),Ref(a),βs,4)/(6V)^4;
 
 CV = @. 6V*(E2 - E1^2)
 plot(βs, CV)
